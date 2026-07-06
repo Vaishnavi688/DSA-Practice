c3e6f4f6a147ace1fa2366de7f6c11d214d81701
Vaishnavi Annam <vaishnavi.annam2011@gmail.com>
2026-07-06 23:43:53 +0530

    ShortestPath and Substring

--- Strings/Strings.java
+++ Strings/Strings.java
@@ -13,6 +13,40 @@ public class Strings {
         }
         return true;
     }
+    public static float getShortestPath(String path) {
+        int x = 0; int y = 0;
+        for(int i=0; i<path.length(); i++) {
+            char dir = path.charAt(i);
+            //South
+            if(dir == 'S'){
+                y--;
+            }
+            //North
+            else if(dir =='N'){
+                y++;
+            }
+            //East
+            else if(dir == 'E'){
+                x++;
+            }
+            //West
+            else {
+                x--;
+            }
+            
+        }
+        int X2 = x*x;
+        int Y2 = y*y;
+        return (float)Math.sqrt(X2+Y2);
+    }
+    public static String substr(String str, int si, int ei) {
+        String substr = "";
+        for(int i=si; i<ei; i++) {
+            substr += str.charAt(i);
+
+        }
+        return substr;
+    }
     public static void main (String[] args) {
         String name;
         Scanner sc = new Scanner(System.in);
@@ -22,5 +56,12 @@ public class Strings {
         } else {
             System.out.println("Not a palindrome");
         }
+        String path = sc.nextLine();
+        System.out.println(getShortestPath(path));
+        String str = sc.nextLine();
+        int si = sc.nextInt();
+        int ei = sc.nextInt();
+        System.out.println(str.substring(si, ei));
+        System.out.println(substr(str, si, ei));
     }
 }
