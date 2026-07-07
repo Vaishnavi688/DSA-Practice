38f6ae15c714ce855c5648d3be3f730007922dce
Vaishnavi Annam <vaishnavi.annam2011@gmail.com>
2026-07-07 13:20:50 +0530

    StringBuilder to use as strings are immutable

---
+++ Strings/StringBuilderDemo.java
@@ -0,0 +1,28 @@
+package Strings;
+
+public class StringBuilderDemo {
+    private static String toUpperCase(String str) {
+        StringBuilder sb = new StringBuilder("");
+        char ch = Character.toUpperCase(str.charAt(0));
+        sb.append(ch);
+        for(int i=1; i<str.length(); i++) {
+            if(str.charAt(i) == ' ' && i<str.length()-1) {
+                sb.append(str.charAt(i));
+                i++;
+                sb.append(Character.toUpperCase(str.charAt(i)));
+            }
+            else{
+                sb.append(str.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("");
+        for(char ch = 'a'; ch<='z'; ch++) {
+            sb.append(ch);
+        }
+        System.out.println(sb);
+        System.out.println(toUpperCase("my name is vaishnavi"));
+    }
+}
