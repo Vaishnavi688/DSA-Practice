fb8e91af8df6bf39ec01bedeb59ab1e03b20ef34
Vaishnavi Annam <vaishnavi.annam2011@gmail.com>
2026-07-07 18:23:45 +0530

    String Compression

---
+++ Strings/compression.java
@@ -0,0 +1,24 @@
+package Strings;
+import java.util.*;
+public class compression {
+    public static String compress(String str) {
+        String newstr = "";
+        for(int i=0; i<str.length(); i++){
+            Integer count = 1;
+            while(i<str.length()-1 && str.charAt(i)==str.charAt(i+1)){
+                count++;
+                i++;
+            }
+            newstr += str.charAt(i);
+            if(count>1){
+                newstr += count.toString();
+            }
+        }
+        return newstr;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.print(compress(s));
+    }
+}
